42aba450de056a8814ce3aead571612c44486e6d
Alexander Gerasimov <aleksandr.gerasimov.05@gmail.com>
2026-02-06 19:37:31 +0300

    Implemented MVP of telegram bot and connected with backend

---
+++ bot/config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TG_BOT_KEY = os.getenv("TG_BOT_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+BACKEND_URL = os.getenv("BACKEND_URL")
+BACKEND_PORT = os.getenv("BACKEND_PORT")
